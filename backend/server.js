218f20795bdca628a39dc3901ad9222f2e46c1f6
Manjunath Gowdar <57346550+Manjunath-Gowdar-au9@users.noreply.github.com>
2021-04-05 14:51:50 +0530

    first pre Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import {notFound,errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js' //mongoose
-
-
 
 import productRoutes from './routes/productRoutes.js' //external routes
 import userRoutes from './routes/userRoutes.js' //external routes
-
 
 dotenv.config()
 
@@ -19,20 +17,33 @@ const app = express()
 // this middleware will alow to accept json data in req.body on controllers file
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('api is running..')
-})
-
+// '/api/route' is simple restful api syntax
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
-// '/api/route' is simple restful api syntax 
+
+// development and production file choosing
+// __dirname is current folder path
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  // creating static folder for express
+  app.use(express.static(path.join(__dirname, '/frontend/build'))) //path of frontend build folder is pointed
+
+  // creating route for production for index.html file of build
+  // select all route which is not '/api' and point it to index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+  //production app can be accessed from localhost:5000 on server start
+} else {
+  app.get('/', (req, res) => {
+    res.send('api is running..')
+  })
+}
 
 // to handle error from 'error_middleware.js' file
 app.use(notFound)
-
 app.use(errorHandler)
-
-
 
 const PORT = process.env.PORT || 5000
 app.listen(
@@ -41,4 +52,3 @@ app.listen(
     `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
-
